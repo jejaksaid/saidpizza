2bbb84f786d75ca8d9d1853d1cc49aeeb0e231f3
Jejak <said@fastlearningcenter.com>
2022-01-07 11:13:44 +0700

    added navbar

--- pages/index.js
+++ pages/index.js
@@ -2,15 +2,21 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
+
 export default function Home() {
   return (
     <div className={styles.container}>
+      
+
       <Head>
         <title>SaidPizza</title>
         <meta name="description" content="Paling mantap se bogor" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
     homepage
+    
+ 
+
     </div>
   )
 }
