f5e893ebc94ded6f88578f31ebc20c3f6381a96d
Jejak <said@fastlearningcenter.com>
2022-01-10 10:16:29 +0700

    added public/img

---
+++ components/Featured.jsx
@@ -0,0 +1,12 @@
+// import styles from "../styles/featured.module.css";
+// import Image from "next/image";
+
+const Featured = () => {
+    return (
+        <div>
+            featured
+        </div>
+    )
+}
+
+export default Featured
